f884286c9b986e9ad5ecb4f4637784d171822175
adipatiarya <aryaadipati2@gmail.com>
2022-08-15 10:42:05 +0700

    add admin template

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -1,55 +1,119 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"> <!-- Favicon-->
+        <title>@yield('title') - {{ config('app.name') }}</title>
+        <meta name="description" content="@yield('meta_description', config('app.name'))">
+        <meta name="author" content="@yield('meta_author', config('app.name'))">
+        @yield('meta')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        {{-- See https://laravel.com/docs/5.5/blade#stacks for usage --}}
+        @stack('before-styles')
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <meta name="description" content="@yield('meta_description', config('app.name'))">
-    <meta name="author" content="@yield('meta_author', config('app.name'))">
-    @yield('meta')
+        <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/vendor/font-awesome/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/vendor/jvectormap/jquery-jvectormap-2.0.3.min.css') }}"/>
+        <link rel="stylesheet" href="{{ asset('assets/vendor/morrisjs/morris.min.css') }}" />
 
-    @stack('before-styles')
-    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/font-awesome/css/font-awesome.min.css') }}">
-    <!-- Custom Css -->
-    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/color_skins.css') }}">
-    @stack('after-styles')
+        <!-- Custom Css -->
+        <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/color_skins.css') }}">
 
-    @if (trim($__env->yieldContent('page-styles')))
-        @yield('page-styles')
-    @endif
-</head>
-<body class="theme-cyan">
-    <div class="page-loader-wrapper">
-        <div class="loader">
-            <div class="m-t-30"><img src="{{url('/')}}/assets/img/logo-icon.svg" width="48" height="48" alt="Lucid"></div>
-            <p>Please wait...</p>        
-        </div>
-    </div>
-    <div id="wrapper">
-        @include('backend.layouts.navbar')
-        @include('backend.layouts.sidebar')
+       
 
-        <div id="main-content">
-            <div class="container-fluid">
-                @yield('content')
+        @stack('after-styles')
+
+        @if (trim($__env->yieldContent('page-styles')))
+            @yield('page-styles')
+        @endif
+
+    </head>
+    
+    <?php 
+        $setting = !empty($_GET['theme']) ? $_GET['theme'] : '';
+        $theme = "theme-cyan";
+        $menu = "";
+        if ($setting == 'p') {
+            $theme = "theme-purple";
+        } else if ($setting == 'b') {
+            $theme = "theme-blue";
+        } else if ($setting == 'g') {
+            $theme = "theme-green";
+        } else if ($setting == 'o') {
+            $theme = "theme-orange";
+        } else if ($setting == 'bl') {
+            $theme = "theme-blush";
+        } else {
+             $theme = "theme-cyan";
+        }
+
+    ?>
+
+    <body class="<?= $theme ?>">
+
+        <!-- Page Loader -->
+        <div class="page-loader-wrapper">
+            <div class="loader">
+                <div class="m-t-30"><img src="{{url('/')}}/assets/img/logo-icon.svg" width="48" height="48" alt="Lucid"></div>
+                <p>Please wait...</p>        
             </div>
         </div>
 
-    </div>
+        <div id="wrapper">
 
-    @stack('before-scripts')
-    <script src="{{ asset('assets/bundles/libscripts.bundle.js') }}"></script>    
-    <script src="{{ asset('assets/bundles/vendorscripts.bundle.js') }}"></script>
-    @stack('after-scripts')
-    
-    @if (trim($__env->yieldContent('page-script')))
-        @yield('page-script')
-    @endif
-</body>
+            @include('backend.layouts.navbar')
+            @include('backend.layouts.sidebar')
+
+            <div id="main-content">
+                <div class="container-fluid">
+                    <div class="block-header">
+                        <div class="row">
+                            <div class="col-lg-5 col-md-8 col-sm-12">                        
+                                <h2><a href="javascript:void(0);" class="btn btn-xs btn-link btn-toggle-fullwidth"><i class="fa fa-arrow-left"></i></a> @yield('title')</h2>
+                                <ul class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#"><i class="icon-home"></i></a></li>
+                                    @if (trim($__env->yieldContent('parentPageTitle')))
+                                       <li class="breadcrumb-item">@yield('parentPageTitle')</li>
+                                    @endif
+                                    @if (trim($__env->yieldContent('title')))
+                                        <li class="breadcrumb-item active">@yield('title')</li>
+                                    @endif
+                                </ul>
+                            </div>            
+                            
+                        </div>
+                    </div>
+                    
+                    @yield('content')
+
+                </div>
+            </div>
+
+        </div>
+
+        <!-- Scripts -->
+        @stack('before-scripts')
+
+        <script src="{{ asset('assets/bundles/libscripts.bundle.js') }}"></script>    
+        <script src="{{ asset('assets/bundles/vendorscripts.bundle.js') }}"></script>
+        
+        <script src="{{ asset('assets/bundles/morrisscripts.bundle.js') }}"></script><!-- Morris Plugin Js -->
+        <script src="{{ asset('assets/bundles/jvectormap.bundle.js') }}"></script> <!-- JVectorMap Plugin Js -->
+        <script src="{{ asset('assets/bundles/knob.bundle.js') }}"></script>
+        
+
+        <script src="{{ asset('assets/bundles/mainscripts.bundle.js') }}"></script>
+
+        @stack('after-scripts')
+
+        @if (trim($__env->yieldContent('page-script')))
+            
+                @yield('page-script')
+            
+		@endif
+    </body>
 </html>
--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,1 +1,265 @@
-<p>SIDEBAR</p>
+<div id="left-sidebar" class="sidebar">
+    <div class="sidebar-scroll">
+        <div class="user-account">
+            <img src="{{ asset('assets/img/user.png') }}" class="rounded-circle user-photo" alt="User Profile Picture">
+            <div class="dropdown">
+                <span>Welcome,</span>
+                <a href="javascript:void(0);" class="dropdown-toggle user-name" data-toggle="dropdown"><strong>Alizee Thomas</strong></a>
+                <ul class="dropdown-menu dropdown-menu-right account">
+                    <li><a href="#"><i class="icon-user"></i>My Profile</a></li>
+                    <li><a href="#"><i class="icon-envelope-open"></i>Messages</a></li>
+                    <li><a href="javascript:void(0);"><i class="icon-settings"></i>Settings</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#"><i class="icon-power"></i>Logout</a></li>
+                </ul>
+            </div>
+            <hr>
+            <ul class="row list-unstyled">
+                <li class="col-4">
+                    <small>Sales</small>
+                    <h6>456</h6>
+                </li>
+                <li class="col-4">
+                    <small>Order</small>
+                    <h6>1350</h6>
+                </li>
+                <li class="col-4">
+                    <small>Revenue</small>
+                    <h6>$2.13B</h6>
+                </li>
+            </ul>
+        </div>
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs">
+            <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#menu">Menu</a></li>
+            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#Chat"><i class="icon-book-open"></i></a></li>
+            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#setting"><i class="icon-settings"></i></a></li>
+            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#question"><i class="icon-question"></i></a></li>                
+        </ul>
+            
+        <!-- Tab panes -->
+        <div class="tab-content p-l-0 p-r-0">
+            <div class="tab-pane active" id="menu">
+                <nav id="left-sidebar-nav" class="sidebar-nav">
+                    <ul id="main-menu" class="metismenu">                            
+                        <li class="{{ Request::segment(1) === 'dashboard' ? 'active' : null }}">
+                            <a href="#Dashboard" class="has-arrow"><i class="icon-home"></i> <span>Dashboard</span></a>
+                            
+                        </li>
+                        <li class="{{ Request::segment(1) === 'app' ? 'active' : null }}">
+                            <a href="#App" class="has-arrow"><i class="icon-grid"></i> <span>App</span></a>
+                            
+                        </li>
+                        <li class="{{ Request::segment(1) === 'file-manager' ? 'active' : null }}">
+                            <a href="#FileManager" class="has-arrow"><i class="icon-folder"></i> <span>File Manager</span></a>
+                            
+                        </li>
+                        <li class="{{ Request::segment(1) === 'blog' ? 'active' : null }}">
+                            <a href="#Blog" class="has-arrow"><i class="icon-globe"></i> <span>Blog</span></a>
+                            
+                        </li>
+                        <li class="{{ Request::segment(1) === 'ui-elements' ? 'active' : null }}">
+                            <a href="#uiElements" class="has-arrow"><i class="icon-diamond"></i> <span>UI Elements</span></a>
+                           
+                        </li>
+                        <li class="{{ Request::segment(1) === 'widgets' ? 'active' : null }}">
+                            <a href="#Widgets" class="has-arrow"><i class="icon-puzzle"></i> <span>Widgets</span></a>
+                            
+                        </li>
+                        <li class="{{ Request::segment(1) === 'authentication' ? 'active' : null }}">
+                            <a href="#Authentication" class="has-arrow"><i class="icon-lock"></i> <span>Authentication</span></a>
+                           
+                        </li>
+                        <li class="{{ Request::segment(1) === 'pages' ? 'active' : null }}">
+                            <a href="#Pages" class="has-arrow"><i class="icon-docs"></i> <span>Pages</span></a>
+                            
+                        </li>
+                        <li class="{{ Request::segment(1) === 'forms' ? 'active' : null }}">
+                            <a href="#forms" class="has-arrow"><i class="icon-pencil"></i> <span>Forms</span></a>
+                            
+                        </li>
+                        <li class="{{ Request::segment(1) === 'table' ? 'active' : null }}">
+                            <a href="#Tables" class="has-arrow"><i class="icon-tag"></i> <span>Tables</span></a>
+                            
+                        </li>
+                        <li class="{{ Request::segment(1) === 'charts' ? 'active' : null }}">
+                            <a href="#charts" class="has-arrow"><i class="icon-bar-chart"></i> <span>Charts</span></a>
+                           
+                        </li>
+                        <li class="{{ Request::segment(1) === 'map' ? 'active' : null }}">
+                            <a href="#Maps" class="has-arrow"><i class="icon-map"></i> <span>Maps</span></a>
+                            
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="tab-pane p-l-15 p-r-15" id="Chat">
+                <form>
+                    <div class="input-group m-b-20">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" ><i class="icon-magnifier"></i></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Search...">
+                    </div>
+                </form>
+                <ul class="right_chat list-unstyled">
+                    <li class="online">
+                        <a href="javascript:void(0);">
+                            <div class="media">
+                                <img class="media-object " src="{{ asset('assets/img/xs/avatar4.jpg') }}" alt="">
+                                <div class="media-body">
+                                    <span class="name">Chris Fox</span>
+                                    <span class="message">Designer, Blogger</span>
+                                    <span class="badge badge-outline status"></span>
+                                </div>
+                            </div>
+                        </a>                            
+                    </li>
+                    <li class="online">
+                        <a href="javascript:void(0);">
+                            <div class="media">
+                                <img class="media-object " src="{{ asset('assets/img/xs/avatar5.jpg') }}" alt="">
+                                <div class="media-body">
+                                    <span class="name">Joge Lucky</span>
+                                    <span class="message">Java Developer</span>
+                                    <span class="badge badge-outline status"></span>
+                                </div>
+                            </div>
+                        </a>                            
+                    </li>
+                    <li class="offline">
+                        <a href="javascript:void(0);">
+                            <div class="media">
+                                <img class="media-object " src="{{ asset('assets/img/xs/avatar2.jpg') }}" alt="">
+                                <div class="media-body">
+                                    <span class="name">Isabella</span>
+                                    <span class="message">CEO, Thememakker</span>
+                                    <span class="badge badge-outline status"></span>
+                                </div>
+                            </div>
+                        </a>                            
+                    </li>
+                    <li class="offline">
+                        <a href="javascript:void(0);">
+                            <div class="media">
+                                <img class="media-object " src="{{ asset('assets/img/xs/avatar1.jpg') }}" alt="">
+                                <div class="media-body">
+                                    <span class="name">Folisise Chosielie</span>
+                                    <span class="message">Art director, Movie Cut</span>
+                                    <span class="badge badge-outline status"></span>
+                                </div>
+                            </div>
+                        </a>                            
+                    </li>
+                    <li class="online">
+                        <a href="javascript:void(0);">
+                            <div class="media">
+                                <img class="media-object " src="{{ asset('assets/img/xs/avatar3.jpg') }}" alt="">
+                                <div class="media-body">
+                                    <span class="name">Alexander</span>
+                                    <span class="message">Writter, Mag Editor</span>
+                                    <span class="badge badge-outline status"></span>
+                                </div>
+                            </div>
+                        </a>                            
+                    </li>                        
+                </ul>
+            </div>
+            <div class="tab-pane p-l-15 p-r-15" id="setting">
+                <h6>Choose Skin</h6>
+                <ul class="choose-skin list-unstyled">
+                    <li data-theme="purple">
+                        <div class="purple"></div>
+                        <span>Purple</span>
+                    </li>                   
+                    <li data-theme="blue">
+                        <div class="blue"></div>
+                        <span>Blue</span>
+                    </li>
+                    <li data-theme="cyan" class="active">
+                        <div class="cyan"></div>
+                        <span>Cyan</span>
+                    </li>
+                    <li data-theme="green">
+                        <div class="green"></div>
+                        <span>Green</span>
+                    </li>
+                    <li data-theme="orange">
+                        <div class="orange"></div>
+                        <span>Orange</span>
+                    </li>
+                    <li data-theme="blush">
+                        <div class="blush"></div>
+                        <span>Blush</span>
+                    </li>
+                </ul>
+                <hr>
+                <h6>General Settings</h6>
+                <ul class="setting-list list-unstyled">
+                    <li>
+                        <label class="fancy-checkbox">
+                            <input type="checkbox" name="checkbox">
+                            <span>Report Panel Usag</span>
+                        </label>
+                    </li>
+                    <li>
+                        <label class="fancy-checkbox">
+                            <input type="checkbox" name="checkbox" checked>
+                            <span>Email Redirect</span>
+                        </label>
+                    </li>
+                    <li>
+                        <label class="fancy-checkbox">
+                            <input type="checkbox" name="checkbox" checked>
+                            <span>Notifications</span>
+                        </label>                      
+                    </li>
+                    <li>
+                        <label class="fancy-checkbox">
+                            <input type="checkbox" name="checkbox">
+                            <span>Auto Updates</span>
+                        </label>
+                    </li>
+                    <li>
+                        <label class="fancy-checkbox">
+                            <input type="checkbox" name="checkbox">
+                            <span>Offline</span>
+                        </label>
+                    </li>
+                    <li>
+                        <label class="fancy-checkbox">
+                            <input type="checkbox" name="checkbox">
+                            <span>Location Permission</span>
+                        </label>
+                    </li>
+                </ul>
+            </div>
+            <div class="tab-pane p-l-15 p-r-15" id="question">
+                <form>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" ><i class="icon-magnifier"></i></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Search...">
+                    </div>
+                </form>
+                <ul class="list-unstyled question">
+                    <li class="menu-heading">HOW-TO</li>
+                    <li><a href="javascript:void(0);">How to Create Campaign</a></li>
+                    <li><a href="javascript:void(0);">Boost Your Sales</a></li>
+                    <li><a href="javascript:void(0);">Website Analytics</a></li>
+                    <li class="menu-heading">ACCOUNT</li>
+                    <li><a href="javascript:void(0);">Cearet New Account</a></li>
+                    <li><a href="javascript:void(0);">Change Password?</a></li>
+                    <li><a href="javascript:void(0);">Privacy &amp; Policy</a></li>
+                    <li class="menu-heading">BILLING</li>
+                    <li><a href="javascript:void(0);">Payment info</a></li>
+                    <li><a href="javascript:void(0);">Auto-Renewal</a></li>                        
+                    <li class="menu-button m-t-30">
+                        <a href="javascript:void(0);" class="btn btn-primary"><i class="icon-question"></i> Need Help?</a>
+                    </li>
+                </ul>
+            </div>                
+        </div>          
+    </div>
+</div>
