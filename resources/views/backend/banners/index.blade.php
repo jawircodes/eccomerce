2befec9a636d203ef5b415a596a97cf1e832f762
adipatiarya <aryaadipati2@gmail.com>
2022-08-18 21:49:35 +0700

    Menambahkan dependenci unisharp/laravel-filemanager

---
+++ resources/views/backend/banners/index.blade.php
@@ -0,0 +1,37 @@
+@extends('backend.layouts.master')
+@section('title', 'Banners Management')
+@section('parentPageTitle', 'Admin')
+@section('childPageTitle', 'All Banners')
+
+@section('content')
+
+<div class="row clearfix">
+
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="header">
+                <h2>Basic Table <small>Basic example without any additional modification classes</small> </h2>                            
+            </div>
+            <div class="body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover js-basic-example dataTable table-custom">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Photo</th>
+                                <th>Condition</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+@endsection
